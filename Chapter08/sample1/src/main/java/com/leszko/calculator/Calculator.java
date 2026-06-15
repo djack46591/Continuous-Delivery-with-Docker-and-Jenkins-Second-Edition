48efc7ede42fbb0c01ae3c4185a8aaa815813377
David Jackson <djack465@gmail.com>
2026-06-15 17:32:31 -0400

    Fixed Checkstyle error by adding Javadoc comment to main Calculator class

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,11 +3,15 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+/**
+ * Main Calculator service.
+ */
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
-	@Cacheable("sum")
-	public int sum(int a, int b) {
-		return a + b;
-	}
+        
+        @Cacheable("sum")
+        public int sum(int a, int b) {
+                return a + b;
+        }
 }
