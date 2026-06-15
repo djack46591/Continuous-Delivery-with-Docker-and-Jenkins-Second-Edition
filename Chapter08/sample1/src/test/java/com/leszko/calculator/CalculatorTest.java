eef8eba5daeb930473b0f8b425a2cb3b3ecea41e
David Jackson <djack465@gmail.com>
2026-06-15 17:28:30 -0400

    Fixed Checkstyle error by adding Javadoc comment to CalculatorTest

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -2,6 +2,9 @@ package com.leszko.calculator;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Tests for the Calculator class.
+ */
 public class CalculatorTest {
      private Calculator calculator = new Calculator();
 
